e33f8a5fbf512a6f5d9794882d54979a19bdc0d4
yanivshani100 <yaniv.shani100@gmail.com>
2022-05-18 18:57:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,70 @@
+'use strict';
 
-require('lightrun').start({
-      lightrunSecret: 'b0ec4ae2-e711-4ad5-98d9-f2ac028c093e',
-      company: '27f1f863-c035-4711-9479-ab09f37aef3a',
-  });
 
-const express = require("express");
+require('/Users/yanivshani/lightrun/athena-dev/nodejs-agent').start({
+  lightrunSecret: 'b0ec4ae2-e711-4ad5-98d9-f2ac028c093e',
+  capture: {includeNodeModules:true},
+  company: 'yanivshani',
+});
+
+const os = require("os");
+const hostname = os.hostname();
+const express = require('express');
+
+console.log('os.hostname()='+os.hostname());
+console.log('process.version='+process.version);
+console.log('process.version='+process.platform);
+console.log('os.release()='+os.release());
+
+
+// Constants
+const PORT = 8083;
+
+// App
 const app = express();
-const port = process.env.PORT || 3001;
+app.get('/', (req, res) => {
+  res.send(`${ new Date().toISOString() }: Hello from ${ hostname } demo for NodeJS`);
 
-app.get("/", (req, res) => res.type('html').send(html));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT);
+console.log('Server up and running');
 
+const MAX_NUMBER = 10 ** 9;
+let cnt = 0
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+function isPrime(num) {
+    if (num === 2) {
+        return true;
+    }
+    if (num < 2 || num % 2 === 0) {
+        return false;
+    }
+    for (let i = 3; i * i <= num; i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+async function countPrimes(i) {
+    if (isPrime(i)) {
+        cnt++;
+        await sleep(1000);
+    }
+    if (i < MAX_NUMBER) {
+        setTimeout(() => countPrimes(++i));
+    } else {
+        console.log("Total number of primes: " + cnt);
+    }
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+console.log("Starting to count prime numbers...");
+
+setTimeout(() => countPrimes(2));
